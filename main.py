0efaae0fcb1bba3749fd52bbd80dc933c93a9986
orisun <orisunzhang@hotmail.com>
2022-12-06 22:43:34 +0800

    readme

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from model import train
 from backtest import back_test
 
 if __name__ == '__main__':
-    fetch_data('20220101', '20221130')
-    gen_corpus()
-    train()
-    back_test('20220920', '20221120', os.path.join("file", "record.csv"))
+    fetch_data('20220101', '20221130')  # 调tushare，获取一段时间内所有股票的交易数据
+    gen_corpus()  # 生成训练样本
+    train()  # 训练模型
+    back_test('20220920', '20221120', os.path.join("file", "record.csv"))  # 回测
